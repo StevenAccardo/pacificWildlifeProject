a3419818520dd6cce1a8cd5ed67d1f4d682a70b4
Steven Accardo <accardo.steven@gmail.com>
2018-07-25 10:00:05 -0700

    cropped imagaes with GIMP, and add them to coursel

--- src/components/landing/carousel.js
+++ src/components/landing/carousel.js
@@ -1,27 +1,27 @@
 import React, { Component } from 'react';
 import { Carousel } from 'react-responsive-carousel';
-import pelican from '../../../assets/images/tackleBox.jpg';
+import pelican from '../../../assets/images/pelican.jpg';
+import babySQL from '../../../assets/images/bbySql.jpg';
+import seagull from '../../../assets/images/seagull.jpg';
+import racoon from '../../../assets/images/racoon.jpg';
 import styles from 'react-responsive-carousel/lib/styles/carousel.min.css';
 
 export default class extends React.Component {
   render() {
     return (
       <div className="carouselContainer">
-        <Carousel showThumbs={false}>
+        <Carousel showIndicators={false} showStatus={false} autoPlay infiniteLoop showThumbs={false} emulateTouch>
           <div>
             <img className="baby" src={pelican} />
           </div>
           <div>
-            <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide3" />
+            <img className="baby" src={babySQL} />
           </div>
           <div>
-            <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide4" />
+            <img className="baby" src={seagull} />
           </div>
           <div>
-            <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide5" />
-          </div>
-          <div>
-            <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide6" />
+            <img className="baby" src={racoon} />
           </div>
         </Carousel>
       </div>
